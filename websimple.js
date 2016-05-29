4c11b3a84c3978ba2e71cba74f8bbce1b877938a
GIRARDIN Lorenzo <lorenzo.girardin@socomec.com>
2016-05-29 22:37:14 +0200

    webserver simple creation

---
+++ websimple.js
@@ -0,0 +1,41 @@
+
+var http = require('http');
+var util = require('util');
+
+var obj = { action : "test", version: 1.0    };
+ 
+function processa(request, response)
+{
+   if(conCount <= 3)
+   {	
+	console.log("Incoming request method " + request.method + " asking for " + request.url);
+	var data =  JSON.stringify(obj);//"Mi hai chiamato, sono qui";
+	
+	var data_len = data.length;
+	response.writeHead("200", { "Content-Lenght":data_len, "Content-Type":"text/plain" });
+	response.end(data);
+	}
+	else
+	{
+		response.writeHead("500", {"Content-Type":"text/plain" });
+		response.end("Max number of connection reached");
+	}
+}
+
+
+var web = http.createServer(processa);
+web.listen(8383);
+
+var conCount = 0;
+web.on("connection", function(stream)
+{
+	console.log("Connection established :" + conCount++);
+	//console.log(util.inspect(stream));
+});
+/*
+web.once("connection", function(stream)
+{
+	console.log("1^ Connection");
+	console.log(util.inspect(stream));
+});
+*/
